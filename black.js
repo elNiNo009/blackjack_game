f4a3b9f9b8f2ab44bab667dfbb4359c8b2a0ca78
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-02-15 18:46:08 +0530

    random card generator

--- black.js
+++ black.js
@@ -1,13 +1,42 @@
 
-let firstcard=6
-let secondcard=1
+let firstcard=randomcard()
+let secondcard=randomcard()
 
 let sum=firstcard+secondcard
 let message=""
 console.log(sum)
 let messageEl=document.getElementById("message-el")
-function startGame()
+let sumEl=document.querySelector("#sum-el")
+let cardEl=document.querySelector("#card-el")
+let cards=[firstcard,secondcard]
+
+function randomcard()
 {
+let num= Math.floor(Math.random()*13+1)
+if(num===1)
+return 11
+else if(num===11 || num===12 || num===13)
+return 10
+else
+return num
+}
+
+function startGame()
+ {
+Game()
+}
+
+
+function Game()
+{
+  sumEl.textContent="Sum : "+sum
+
+  cardEl.textContent="Cards :"
+  for (let i=0; i< cards.length; i++)
+  {
+    cardEl.textContent+= cards[i] + " "
+  }
+
 if(sum<21)
 {
   message="draw"
@@ -25,7 +54,18 @@ else
 //console.log("you out");
 
 }
+
 messageEl.textContent=message
+}
+
+
+
+function newCard()
+{
+let card=randomcard();
+sum+=card
+cards.push(card)
+Game()
 }
 //messageEl.textContent=
  //console.log(message)
